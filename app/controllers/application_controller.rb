ebdf0e4f8f8d4ddcff841d3a53ec0cb7067271ab
Yokanman <puresoulhatto@gmail.com>
2021-02-07 12:58:05 +0900

    first_commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :gender_id, :hobby, :comment, :age, :prefecture_id, :city, :address])
+  end
+
+end
