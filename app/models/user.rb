73c07fd731d8f79cad2d73899aa382bb13f5b6cc
makiwakurin <takahiro1608@gmail.com>
2021-02-14 09:26:15 +0900

    active_hashにて作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  with_options presence: true do
+    validates :nickname
+    validates :gender_id
+    validates :hobby
+    validates :comment
+    validates :age
+    validates :prefecture_id
+    validates :city
+    validates :address
+  end
 end
