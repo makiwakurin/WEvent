78ad502488410630873c5e32028831b037f3d637
makiwakurin <takahiro1608@gmail.com>
2021-02-24 17:04:44 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'events#index'
   resources :events, only: [:index, :new, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms, only: [:new, :create]
 end
