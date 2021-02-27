5aecf475aa8e6b5883cacdae005b7b8820f6f3ec
Latteholic <cafelatte@gmail.com>
2021-02-28 02:49:47 +0900

    タスクの一覧表作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,16 @@
 class TasksController < ApplicationController
+  before_action :define_event, exept: :destroy
   def index
-    @tasks = Tasks.all
+    @tasks = @event.tasks
+  end
+
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :description, :deadline, :user_id).merge(event_id: params[:event_id])
+  end
+
+  def define_event
+    @event = Event.find(params[:event_id])
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  attr_accessor :user_id, :event_id
   belongs_to :user
   belongs_to :event
 
