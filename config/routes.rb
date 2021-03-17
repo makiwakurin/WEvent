f67e6cbdd705d259a8d7f69382d3f256eec81b93
makiwakurin <takahiro1608@gmail.com>
2021-03-17 10:31:48 +0900

    リセットcssの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   devise_for :users
   root to: 'events#index'
   resources :events, only: [:index, :new, :create]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :new, :create]
+  end
+
 end
