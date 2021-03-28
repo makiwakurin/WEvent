14ae63658cbcc92c566b238a66b9f7b47b0535d3
makiwakurin <takahiro1608@gmail.com>
2021-03-28 20:29:02 +0900

    コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'events#index'
-  resources :events, only: [:index, :new, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :events, only: [:index, :new, :create] do
+    resources :tasks
+  end
+  
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :new, :create]
+  end
 end
+
